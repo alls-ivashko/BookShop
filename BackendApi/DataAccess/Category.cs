9af6fff19b63c1f824fc3641d11fe13d8661adad
Aleksandra Ivashko <alls.ivashko@gmail.com>
2023-05-06 09:37:01 +0300

    Миграция моделей базы данных в проект

---
+++ BackendApi/DataAccess/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAccess
+{
+    public partial class Category
+    {
+        public Category()
+        {
+            Books = new HashSet<Book>();
+            CategoryAttributes = new HashSet<CategoryAttribute>();
+        }
+
+        public string Name { get; set; } = null!;
+        public bool Deleted { get; set; }
+
+        public virtual ICollection<Book> Books { get; set; }
+        public virtual ICollection<CategoryAttribute> CategoryAttributes { get; set; }
+    }
+}
---
+++ BackendApi/DataAccess/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAccess
+{
+    public partial class Customer
+    {
+        public Customer()
+        {
+            Carts = new HashSet<Cart>();
+        }
+
+        public string Login { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string? LastName { get; set; }
+        public string? FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public string PhoneNumber { get; set; } = null!;
+        public int? ZipCode { get; set; }
+        public string? Region { get; set; }
+        public string? City { get; set; }
+        public string? Street { get; set; }
+        public int? House { get; set; }
+        public int? Flat { get; set; }
+        public int Age { get; set; }
+        public bool Gender { get; set; }
+        public bool Deleted { get; set; }
+
+        public virtual ICollection<Cart> Carts { get; set; }
+    }
+}
