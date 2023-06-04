9d1091bac2c5fba73809b4945bd47e391bbd65d6
Aleksandra Ivashko <alls.ivashko@gmail.com>
2023-06-04 10:28:11 +0300

    Task 10

---
+++ BackendApi/Web/Program.cs
@@ -0,0 +1,53 @@
+using BusinessLogic.Services;
+using DataAccess.Wrapper;
+using DataAccess;
+using Domain.Interfaces;
+using Domain.Wrapper;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
+using Web.Data;
+
+namespace Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<BookShopContext>(
+                optionsAction: options => options.UseSqlServer(
+                    connectionString: "Server=DESKTOP-QNNF9FJ; Database=BookShopNew;Trusted_Connection=true;"));
+
+            builder.Services.AddScoped<IRepositoryWrapper, RepositoryWrapper>();
+            builder.Services.AddScoped<IUserService, UserService>();
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+            builder.Services.AddServerSideBlazor();
+            builder.Services.AddSingleton<WeatherForecastService>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.MapBlazorHub();
+            app.MapFallbackToPage("/_Host");
+
+            app.Run();
+        }
+    }
+}
